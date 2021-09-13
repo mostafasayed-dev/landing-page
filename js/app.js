3996732b52dec378206495eeba9689575ddcec26
mostafasayed-dev <mostfasayed.fathi@gmail.com>
2021-09-13 14:30:51 +0200

    Enhancing scroll / Adding scroll to top feature

--- js/app.js
+++ js/app.js
@@ -4,13 +4,35 @@ let sections = document.querySelectorAll('section');
 // build the nav
 function buildDynamicMenu(){
     let nav = document.querySelector('ul');
+    let index = 1;
     for (const section of sections) {
         let navItem = document.createElement('li');
+        navItem.setAttribute('id', 'item'+index);
         navItem.innerHTML = `<a class='menu__link' 
                                 href='#${section.getAttribute('id')}'>
                                 ${section.getAttribute('data-nav')}
                             </a>`;
+        navItem.addEventListener('click', function(event){
+            event.preventDefault();
+            section.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+            setMenuItemActive(navItem.getAttribute('id'));
+        });
         nav.appendChild(navItem);
+        index++;
+    }
+}
+
+function setMenuItemActive(navItemId){
+    let navItems = document.querySelectorAll('li');
+    for (const item of navItems) {
+        let itemId = item.getAttribute('id');
+        if(navItemId == itemId){
+            if(!item.classList.contains('link__active')){
+                item.classList.add('link__active');
+            }
+        }else{
+            item.classList.remove('link__active');
+        }
     }
 }
 
@@ -19,14 +41,23 @@ function setCurrentSectionActive(){
     for (const section of sections) {
         // to get section position
         let sectionPosition = section.getBoundingClientRect(); 
-        if(sectionPosition.top >= 0){ // section near top of viewport
+        if( sectionPosition.top + (sectionPosition.height / 2) >= 0 && 
+            sectionPosition.bottom <= sectionPosition.height){ // section in viewport
             if(!section.classList.contains('your-active-class')){
                 section.classList.add('your-active-class');// add active class to this section
+                let sectionId = section.getAttribute('id');
+                sectionId = sectionId.replace('section', '');
+                setMenuItemActive('item'+sectionId);
             }
         } else {
             section.classList.remove('your-active-class');
         }
     }
+}
+
+function scrollToTop(){
+    // scroll to first section
+    sections[0].scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
 }
 
 // Build dynamic menu 
